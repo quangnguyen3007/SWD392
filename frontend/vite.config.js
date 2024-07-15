4358324265ec775eb2e6ce5c6003ecf42d6009ce
quangnguyen3007 <nguyenducquang3007@gmail.com>
2024-07-16 00:40:07 +0700

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api/": "https://babyboo-backend-test.onrender.com",
-  //     "/uploads/": "https://babyboo-backend-test.onrender.com",
-  //   },
-  // },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/": "https://babyboo-backend-test.onrender.com",
+      "/uploads/": "https://babyboo-backend-test.onrender.com",
+    },
+  },
 });
