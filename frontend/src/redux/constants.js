93b3fc4309671f4f45a0d701436c184c594922ca
quangnguyen3007 <nguyenducquang3007@gmail.com>
2024-07-16 00:31:58 +0700

    testDeploy

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,13 +1,7 @@
 export const BASE_URL = import.meta.env.VITE_API_URL || "";
-
-export const USERS_URL = `${BASE_URL}/api/users`;
-export const CATEGORY_URL = `${BASE_URL}/api/category`;
-export const PRODUCT_URL = `${BASE_URL}/api/products`;
-export const UPLOAD_URL = `${BASE_URL}/api/upload`;
-export const ORDERS_URL = `${BASE_URL}/api/orders`;
-export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;;
-// export const CATEGORY_URL = `${BASE_URL}/api/category`;
-// export const PRODUCT_URL = `${BASE_URL}/api/products`;
-// export const UPLOAD_URL = `${BASE_URL}/api/upload`;
-// export const ORDERS_URL = `${BASE_URL}/api/orders`;
-// export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+export const USERS_URL = `${BASE_URL}/users`;
+export const CATEGORY_URL = `${BASE_URL}/category`;
+export const PRODUCT_URL = `${BASE_URL}/products`;
+export const UPLOAD_URL = `${BASE_URL}/upload`;
+export const ORDERS_URL = `${BASE_URL}/orders`;
+export const PAYPAL_URL = `${BASE_URL}/config/paypal`;
