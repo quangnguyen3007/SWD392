9f887373c16b70f8ed6520ec92d30a5376625168
quangnguyen3007 <nguyenducquang3007@gmail.com>
2024-07-16 01:04:37 +0700

    testdeploy

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,15 +1,15 @@
-export const BASE_URL = API;
+export const BASE_URL = "https://babyboo-backend-test.onrender.com";
 
-// export const USERS_URL = `/api/users`;
-// export const CATEGORY_URL = `/api/category`;
-// export const PRODUCT_URL = `/api/products`;
-// export const UPLOAD_URL = `/api/upload`;
-// export const ORDERS_URL = `/api/orders`;
-// export const PAYPAL_URL = `/api/config/paypal`;;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;;
 
-export const USERS_URL = `${BASE_URL}users`;
-export const CATEGORY_URL = `${BASE_URL}category`;
-export const PRODUCT_URL = `${BASE_URL}products`;
-export const UPLOAD_URL = `${BASE_URL}upload`;
-export const ORDERS_URL = `${BASE_URL}orders`;
-export const PAYPAL_URL = `${BASE_URL}config/paypal`;
+// export const USERS_URL = `${BASE_URL}users`;
+// export const CATEGORY_URL = `${BASE_URL}category`;
+// export const PRODUCT_URL = `${BASE_URL}products`;
+// export const UPLOAD_URL = `${BASE_URL}upload`;
+// export const ORDERS_URL = `${BASE_URL}orders`;
+// export const PAYPAL_URL = `${BASE_URL}config/paypal`;
